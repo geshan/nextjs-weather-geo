ee42f08f3a652aefc95a2481ba106abf0b55ef95
Geshan Manandhar <geshan@simplywallst.com>
2023-06-28 07:28:21 +1000

    Working URL with city query param

--- src/app/api/weather/route.js
+++ src/app/api/weather/route.js
@@ -1,21 +1,16 @@
-import { NextResponse } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
  
 export async function GET(req) {
-  const url = new URL(req.headers.get('x-middleware-rewrite'));
-  const city = url.searchParams.get('city');
-  const country = url.searchParams.get('country');
-  const region = url.searchParams.get('region');
-
+  const url = new URL(req.url);
+  const city = url.searchParams.get('city') || 'London';
   const res = await fetch(`https://api.api-ninjas.com/v1/weather?city=${city}`, {
     headers: {
       'Content-Type': 'application/json',
       'X-API-Key': process.env.API_NINJAS_API_KEY,
     },
   })
-  const data = await res.json();
+  const data = await res.json(); 
   data.city = city;
-  data.country = country;
-  data.region = region;
  
   return NextResponse.json({ data });
 }
