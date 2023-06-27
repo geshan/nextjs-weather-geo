4eb250b659d6e76bf5199cd731327da62760958a
Geshan Manandhar <geshan@simplywallst.com>
2023-06-27 20:51:12 +1000

    fix error

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
-
 
 export default function Home() {
   return (
